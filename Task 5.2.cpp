f3129e467e2e68b854e8ed1c883fe5473f86ff26
vladkusnetsov <103496399+vladkusnetsov@users.noreply.github.com>
2022-04-18 03:43:13 +0300

    Add files via upload

---
+++ Task 5.2.cpp
@@ -0,0 +1,170 @@
+﻿// Task 5.2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+#include "vector"
+#include <iostream>
+#include "algorithm"
+#include <functional>
+
+int random(int n) {
+    return rand() % n;
+}
+
+bool is_simple(int x) {
+
+    if (x < 3) { return false; }
+
+    for (int i = 2; i < x / 2.0; i++) {
+        if (x % i == 0) { return false; };
+    }
+    return true;
+}
+
+int main()
+{
+    std::vector <int> vec;
+    int temp = 0;
+    for (int i = 0; i < 10; i++) {    //1
+        vec.push_back(rand());
+    }
+
+    std::cout << "Insert numbers" << std::endl;
+
+    for (int i = 0; i < 4; i++) {  //2
+        std::cin >> temp;
+        vec.push_back(temp);
+    }
+
+    std::cout << "Init mass: " << std::endl;
+    std::for_each(vec.begin(), vec.end(), [](int x) {std::cout << x << " "; });
+    
+    std::random_shuffle(vec.begin(), vec.end(), std::pointer_to_unary_function<int, int>(random));   //3
+    std::vector<int>::iterator ip;
+    std::cout << std::endl << "Shuffled mass: " << std::endl;
+    std::for_each(vec.begin(), vec.end(), [](int x) {std::cout << x << " "; });
+
+    ip = std::unique(vec.begin(), vec.end());
+
+    vec.resize(std::distance(vec.begin(), ip));   //4
+
+    std::cout << std::endl << "Unique mass: " << std::endl;
+    std::for_each(vec.begin(), vec.end(), [](int x) {std::cout << x << " "; });
+    
+    int count_of_number = 0;  //5
+
+    std::for_each(vec.begin(), vec.end(), [&](int x) {if (x % 2 != 0) { count_of_number += 1; }});
+
+    std::cout <<std::endl << "Count of nechet numbers: " << count_of_number << std::endl; //6
+    
+    auto elem = std::find_if(vec.begin(), vec.end(), is_simple); //!!!!!!!!!!!!!!!!
+    
+    
+    if (elem != vec.end()) {
+        std::cout << "Simple number is: " << *elem << std::endl;    //7
+    }
+    else {
+        std::cout << "No simple number" << std::endl;
+    }
+
+    std::for_each(vec.begin(), vec.end(), [](int x) {return x * x; });
+
+    std::vector<int> vec_2;
+
+    for (int i = 0; i < vec.size(); i++) {
+        vec_2.push_back(rand());
+    }
+
+    int summ_elem = 0;
+
+    std::for_each(vec_2.begin(), vec_2.end(), [&](int x) {summ_elem += x; });
+
+    std::cout << "Summ of elements: " << summ_elem << std::endl;    //8
+
+    std::fill(vec_2.begin(), vec_2.begin() + 2, 1);   //9
+
+    std::cout << std::endl << "replacing with 1 mass: " << std::endl;
+    std::for_each(vec_2.begin(), vec_2.end(), [](int x) {std::cout << x << " "; });
+
+    std::vector<int> vec_3; //!!!!!
+    for (int i = 0; i < vec_2.size(); i++) {
+        vec_3.push_back(vec_2[i] - vec[i]);
+    }
+
+
+    std::cout << std::endl << "divided mass: " << std::endl;  //10
+    std::for_each(vec_3.begin(), vec_3.end(), [](int x) {std::cout << x << " "; });
+
+    auto temp_func = [](int x) {return x < 0;};
+    std::replace_if(vec_3.begin(), vec_3.end(), temp_func, 0);
+    std::cout << std::endl << "replace with zero mass: " << std::endl; //11
+    std::for_each(vec_3.begin(), vec_3.end(), [](int x) {std::cout << x << " "; });
+
+    
+    std::remove(vec_3.begin(), vec_3.end(), 0);
+    std::cout << std::endl << "no zero mass: " << std::endl; //12
+    std::for_each(vec_3.begin(), vec_3.end(), [](int x) {std::cout << x << " "; });
+
+
+    std::reverse(vec_3.begin(), vec_3.end());
+    std::cout << std::endl << "reversed mass: " << std::endl; //13
+    std::for_each(vec_3.begin(), vec_3.end(), [](int x) {std::cout << x << " "; });
+
+    int max_elem;
+    std::vector <int> result;
+    std::cout << std::endl << "Top 3 elem" << std::endl;  //14
+    std::vector<int> vec_3_copy;
+
+    for (int i = 0; i < vec_3.size(); i++)
+        vec_3_copy.push_back(vec_3[i]);
+   
+    for (int i = 0; i < 3; i++) {
+        max_elem = *std::max_element(vec_3.begin(), vec_3.end());
+        std::cout << "Top " << i + 1 << ": " << max_elem << std::endl;
+        std::remove(vec_3.begin(), vec_3.end(), max_elem);
+    }
+
+    std::sort(vec.begin(), vec.end());
+    std::sort(vec_2.begin(), vec_2.end());
+
+    std::cout << std::endl << "sorted 1st mass: " << std::endl; //15
+    std::for_each(vec.begin(), vec.end(), [](int x) {std::cout << x << " "; });
+
+    std::cout << std::endl << "sorted 2nd mass: " << std::endl; //15
+    std::for_each(vec_2.begin(), vec_2.end(), [](int x) {std::cout << x << " "; });
+
+    std::vector<int> vec_4;
+
+    for (auto iter = vec.begin(); iter != vec.end(); iter++) {
+        vec_4.push_back(*iter);
+    }
+
+    for (auto iter = vec_2.begin(); iter != vec_2.end(); iter++) {
+        vec_4.push_back(*iter);
+    }
+
+    std::cout << std::endl << "merged mass: " << std::endl; //15
+    std::for_each(vec_4.begin(), vec_4.end(), [](int x) {std::cout << x << " "; });
+
+
+    std::cout << std::endl << "1st sequence" << std::endl;
+    std::for_each(vec.begin(), vec.end(), [](int x) {std::cout << x << " "; });
+
+    std::cout << std::endl << "2nd sequence" << std::endl;
+    std::for_each(vec_2.begin(), vec_2.end(), [](int x) {std::cout << x << " "; });
+
+    std::cout << std::endl << "3d sequence" << std::endl;
+    std::for_each(vec_3_copy.begin(), vec_3_copy.end(), [](int x) {std::cout << x << " "; });
+
+    std::cout << std::endl << "4th sequence" << std::endl;
+    std::for_each(vec_4.begin(), vec_4.end(), [](int x) {std::cout << x << " "; });
+}
+
+// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
+// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+
+// Советы по началу работы 
+//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
+//   2. В окне Team Explorer можно подключиться к системе управления версиями.
+//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
+//   4. В окне "Список ошибок" можно просматривать ошибки.
+//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
+//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
---
+++ Task 5.cpp
@@ -0,0 +1,327 @@
+﻿#include "string"
+#include <iostream>
+
+
+std::string RANGE_BETWEEN_CHAR = "\n\n";
+int POSE_NPC = 0;
+int ATT_NPC = 0;
+
+
+
+
+int AI_def() {
+    POSE_NPC += 1;
+    return POSE_NPC % 3;
+}
+
+int AI_att() {
+    ATT_NPC += 1;
+    return ATT_NPC % 3;
+}
+
+void viz_attack(int i, int j) {
+    if (i == 0 and j == 0) {
+        std::cout << " * " << "   " << "   " << std::endl;
+        std::cout << " * " << "   " << "   " << std::endl;
+        std::cout << " * " << "   " << "   " << std::endl;
+        return;
+    }
+
+    if (i == 1 and j == 1) {
+        std::cout << "   " << " * " << "   " << std::endl;
+        std::cout << "   " << " * " << "   " << std::endl;
+        std::cout << "   " << " * " << "   " << std::endl;
+        return;
+    }
+    if (i == 2 and j == 2) {
+        std::cout << "   " << "   " << " * " << std::endl;
+        std::cout << "   " << "   " << " * " << std::endl;
+        std::cout << "   " << "   " << " * " << std::endl;
+        return;
+    }
+
+    if (i == 0 and j == 2) {
+        std::cout << " * " << "   " << "   " << std::endl;
+        std::cout << "   " << " * " << "   " << std::endl;
+        std::cout << "   " << "   " << " * " << std::endl;
+        return;
+    }
+
+
+    if (i == 2 and j == 1) {
+        std::cout << "   " << "   " << " * " << std::endl;
+        std::cout << "   " << "   " << " * " << std::endl;
+        std::cout << "   " << " * " << "   " << std::endl;
+        return;
+    }
+
+    if ((i == 2 and j == 0)) {
+        std::cout << "   " << "   " << " * " << std::endl;
+        std::cout << "   " << " * " << "   " << std::endl;
+        std::cout << " * " << "   " << "   " << std::endl;
+        return;
+    }
+
+    if (i == 1 and j == 2) {
+        std::cout << "   " << " * " << "   " << std::endl;
+        std::cout << "   " << " * " << "   " << std::endl;
+        std::cout << "   " << "   " << " * " << std::endl;
+        return;
+    }
+
+    if (i == 0 and j == 1) {
+        std::cout << " * " << "   " << "   " << std::endl;
+        std::cout << "   " << " * " << "   " << std::endl;
+        std::cout << "   " << " * " << "   " << std::endl;
+        return;
+    }
+
+    if (i == 1 and j == 0) {
+        std::cout << "   " << " * " << "   " << std::endl;
+        std::cout << "   " << " * " << "   " << std::endl;
+        std::cout << " * " << "   " << "   " << std::endl;
+        return;
+    }
+
+
+}
+
+class Hero
+{
+private:
+    int health = 100;
+    int streanght = 10;
+    std::string name;
+    int type_of_block = 0;
+
+
+public:
+
+    void viz_pos()
+    {
+        if (type_of_block == 1) {
+            std::cout << std::endl;
+            std::cout << "    " << " * " << "    " << std::endl;
+            std::cout << "    " << "-|-" << "    " << std::endl;
+            std::cout << "    " << "| |" << "    " << std::endl;
+            std::cout << std::endl;
+        }
+
+        if (type_of_block == 2) {
+            std::cout << "    " << "    " << " * " << std::endl;
+            std::cout << "    " << "    " << "-|-" << std::endl;
+            std::cout << "    " << "    " << "| |" << std::endl;
+            std::cout << std::endl;
+            std::cout << std::endl;
+        }
+
+        if (type_of_block == 0) {
+            std::cout << std::endl;
+            std::cout << std::endl;
+            std::cout << " * " << "    " << "    " << std::endl;
+            std::cout << "-|-" << "    " << "    " << std::endl;
+            std::cout << "| |" << "    " << "    " << std::endl;
+            
+        }
+    }
+
+    void set_name(std::string name_char)
+    {
+        name = name_char;
+    }
+
+    void set_pose(int N) {
+
+        if ((N != 0) or (N != 1) or (N != 2)) {
+            type_of_block = 0;
+        }
+
+        type_of_block = N;
+    }
+
+    int get_health()
+    {
+        return health;
+    }
+
+    int get_pose()
+    {
+        return type_of_block;
+    }
+
+    void set_health(int HP)
+    {
+        if (HP < 0)
+        {
+            std::cout << "wrong inpute HP" << std::endl;;
+        }
+
+        health = HP;
+    }
+
+    void set_power(int power)
+    {
+        if (power < 0)
+        {
+            std::cout << "wrong inpute power" << std::endl;
+        }
+        streanght = power;
+    }
+
+    std::string get_name() {
+        return name;
+    }
+
+    void strike(Hero &otherHero, int type_attack)
+    {
+        int damage = 0;
+        if (streanght > 0) {
+            damage += streanght + rand() % streanght;
+        }
+        else {
+            damage += rand() % 10;
+        }
+
+        if (otherHero.get_pose() == type_attack)
+        {
+            if (streanght > 0) {
+                damage += streanght + rand() % streanght;
+            }
+            else {
+                damage += rand() % 10;
+            }
+        }
+        else {
+            damage = rand() % (streanght+1);
+        }
+
+        otherHero.set_health(otherHero.get_health() - damage);
+    }
+
+    void add_cheat(std::string cheat)
+    {
+        if (cheat == "Otl for this homework") {
+            streanght += 10000;
+            health += 10000;
+            std::cout << "cheat is activate" << std::endl;
+        }
+        else {
+            streanght = 1;
+            health = 1;
+            std::cout << "wrong answer, your HP is 1 and your str is 1" << std::endl;
+        }
+    }
+
+};
+
+void manual()
+{
+    std::cout << "Your quest is kill your enemy" << std::endl;
+    std::cout << "Use this number (0, 1, 2) to choose location of your char" << std::endl;
+    std::cout << "For attack you need push (0, 1, 2), only ony number!!" << std::endl;
+    std::cout << "If char HP down low then 0, char deafted" << std::endl;
+
+}
+
+
+int main()
+{
+    manual();
+    Hero Hero_1;
+    Hero Hero_2;
+    std::string name_1;
+    std::string name_2;
+    std::cout << "input your hero name" << std::endl;
+    std::cin >> name_1;
+    Hero_1.set_name(name_1);
+
+    std::cout << "Your enemy name is Math" << std::endl;
+    name_2 = "Math";
+    Hero_2.set_name(name_2);
+    int my_attack = 0;
+    int my_pose = 0;
+    int AI_attack_pos = 0;
+    int AI_def_pos = 0;
+
+    std::string using_cheats = "n";
+    std::string cheat = "n";
+
+    bool auto_flag = false;
+
+    std::cout << "Do you want use cheats? insert y/n" << std::endl;
+    std::cin >> using_cheats;
+    
+    if (using_cheats == "y") {
+        std::cout << "insert your cheat, but beware if it will wrong cheat, you will debafed!" << std::endl;
+        std::cin >> cheat;
+        Hero_1.add_cheat(cheat);
+    }
+
+    std::cout << "Do you want set auto mode? (game will play in auto mode) insert y/n" << std::endl;
+    std::cin >> using_cheats;
+
+    if (using_cheats == "y") { 
+        auto_flag = true;
+    }
+
+    int counter = 0;
+
+    while (Hero_1.get_health() > 0 and Hero_2.get_health() > 0)
+    {
+        std::cout << std::endl;
+        std::cout << "******Battle number:  " << counter << "******" << std::endl;
+        std::cout << Hero_1.get_name() << " HP: " << Hero_1.get_health() << std::endl;
+        Hero_1.viz_pos();
+        std::cout << RANGE_BETWEEN_CHAR << std::endl;
+
+        Hero_2.viz_pos();
+        std::cout << Hero_2.get_name() << " HP: " << Hero_2.get_health() << std::endl;
+
+        if (not auto_flag) {
+            std::cout << "Input new pose (0, 1, 2): ";
+            std::cin >> my_pose;
+            std::cout << "Input type of attack (0, 1, 2): ";
+            std::cin >> my_attack;
+        }
+        else {
+            my_pose = rand()%3;
+            my_attack = rand()%3;
+        }
+        
+        std::cout << std::endl;
+
+        AI_def_pos = AI_def();
+
+        Hero_2.set_pose(AI_def_pos);
+        Hero_1.set_pose(my_pose);
+
+        Hero_1.strike(Hero_2, my_attack);
+        std::cout << "******RAUND 1******" << std::endl;
+
+        std::cout << Hero_1.get_name() << " HP: " << Hero_1.get_health() << std::endl;
+        Hero_1.viz_pos();
+
+        viz_attack(Hero_1.get_pose(), my_attack % 3);
+
+        Hero_2.viz_pos();
+        std::cout << Hero_2.get_name() << " HP: " << Hero_2.get_health() << std::endl;
+        std::cout << "******RAUND 2******" << std::endl;
+
+        AI_attack_pos = AI_att();
+        Hero_2.strike(Hero_1, AI_attack_pos);
+        std::cout << Hero_1.get_name() << " HP: " << Hero_1.get_health() << std::endl;
+        Hero_1.viz_pos();
+        viz_attack(AI_attack_pos, Hero_2.get_pose());
+        Hero_2.viz_pos();
+        std::cout << Hero_2.get_name() << " HP: " << Hero_2.get_health() << std::endl;
+
+        counter++;
+    }
+
+    if (Hero_1.get_health() < 0) {
+        std::cout << "You died (Hero Elf died)" << std::endl;
+    }
+    else {
+        std::cout << "You win" << std::endl;
+    }
+}
